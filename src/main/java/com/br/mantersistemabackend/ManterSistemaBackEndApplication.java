b0f2aa8f850278e23f3f31d65189e4a3a91911e4
Robson <rpresotti6@gmail.com>
2021-06-23 08:21:18 -0300

    Primeiro \commit

---
+++ src/main/java/com/br/mantersistemabackend/ManterSistemaBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.br.mantersistemabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManterSistemaBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManterSistemaBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/br/mantersistemabackend/ManterSistemaBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.mantersistemabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManterSistemaBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
